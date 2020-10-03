a13ed99fdfed21f6755e3328b68e9769ea541292
Licht Takeuchi <licht-t@outlook.jp>
2020-10-03 12:26:39 +0900

    foo

--- tensorflow_addons/custom_ops/layers/cc/kernels/deformable_conv2d_op.cc
+++ tensorflow_addons/custom_ops/layers/cc/kernels/deformable_conv2d_op.cc
@@ -20,12 +20,8 @@ limitations under the License.
 #endif  // GOOGLE_CUDA
 
 #include "tensorflow/core/framework/common_shape_fns.h"
-#include "tensorflow/core/framework/bounds_check.h"
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/register_types.h"
-#include "tensorflow/core/framework/tensor_types.h"
-#include "tensorflow/core/framework/types.h"
-#include "tensorflow/core/util/tensor_format.h"
 #include "tensorflow/core/util/work_sharder.h"
 
 #include "tensorflow_addons/custom_ops/layers/cc/kernels/deformable_conv2d_op.h"
@@ -33,19 +29,25 @@ limitations under the License.
 namespace tensorflow {
     namespace addons {
 
-        using CPUDevice = Eigen::ThreadPoolDevice ;
+        using CPUDevice = Eigen::ThreadPoolDevice;
         using GPUDevice = Eigen::GpuDevice;
 
+        using Shape8D = Eigen::array<Eigen::DenseIndex, 8>;
+        using Shape7D = Eigen::array<Eigen::DenseIndex, 7>;
         using Shape5D = Eigen::array<Eigen::DenseIndex, 5>;
+        using Shape4D = Eigen::array<Eigen::DenseIndex, 4>;
         using Shape3D = Eigen::array<Eigen::DenseIndex, 3>;
         using Shape2D = Eigen::array<Eigen::DenseIndex, 2>;
 
-        template<typename T, int NDIMS = 1, typename IndexType = Eigen::DenseIndex> using EigenTensor = Eigen::Tensor<T, NDIMS, Eigen::RowMajor, IndexType>;
+        template<typename T, int NDIMS = 1, typename IndexType = Eigen::DenseIndex>
+        using EigenTensor = Eigen::Tensor<T, NDIMS, Eigen::RowMajor, IndexType>;
+        template<typename T, int NDIMS = 1, typename IndexType = Eigen::DenseIndex>
+        using EigenConstTensor = Eigen::Tensor<const T, NDIMS, Eigen::RowMajor, IndexType>;
 
         static const int kMaxParallelImgs = 32;
 
         static int get_greatest_divisor_below_bound(int n) {
-            for (int k = kMaxParallelImgs; k > 1; --k) {
+            for (auto k = kMaxParallelImgs; k > 1; --k) {
                 if (n % k == 0) {
                     return k;
                 }
@@ -54,6 +56,113 @@ namespace tensorflow {
         }
 
         namespace functor {
+            template<typename Dtype> Dtype bilinear_interpolate(
+                    EigenConstTensor<Dtype, 2> input_tensor,
+                    Dtype y,
+                    Dtype x) {
+                auto max_height = input_tensor.dimension(0);
+                auto max_width = input_tensor.dimension(1);
+
+                if (y <= -1 || max_height <= y || x <= -1 || max_width <= x) {
+                    return 0;
+                }
+
+                int y_low = floor(y);
+                int x_low = floor(x);
+                int y_high = y_low + 1;
+                int w_high = x_low + 1;
+
+                Dtype v1 = 0;
+                if (y_low >= 0 && x_low >= 0) {
+                    v1 = input_tensor(y_low, x_low);
+                }
+
+                Dtype v2 = 0;
+                if (y_low >= 0 && w_high <= max_width - 1) {
+                    v2 = input_tensor(y_low, w_high);
+                }
+
+                Dtype v3 = 0;
+                if (y_high <= max_height - 1 && x_low >= 0) {
+                    v3 = input_tensor(y_high, x_low);
+                }
+
+                Dtype v4 = 0;
+                if (y_high <= max_height - 1 && w_high <= max_width - 1) {
+                    v4 = input_tensor(y_high, w_high);
+                }
+
+                auto lh = y - y_low;
+                auto lw = x - x_low;
+                auto hh = 1 - lh;
+                auto hw = 1 - lw;
+
+                auto w1 = hh * hw;
+                auto w2 = hh * lw;
+                auto w3 = lh * hw;
+                auto w4 = lh * lw;
+
+                return w1 * v1 + w2 * v2 + w3 * v3 + w4 * v4;
+            }
+
+            template<typename Dtype> void deformable_im2col(
+                    EigenConstTensor<Dtype, 4> input_tensor,
+                    EigenConstTensor<Dtype, 7> offset_tensor,
+                    EigenConstTensor<Dtype, 6> mask_tensor,
+                    EigenTensor<Dtype, 4> column_buffer_tensor,
+                    int32 input_channels,
+                    int32 filter_rows,
+                    int32 filter_cols,
+                    int32 padding_rows,
+                    int32 padding_cols,
+                    int32 stride_rows,
+                    int32 stride_cols,
+                    int32 dilation_rows,
+                    int32 dilation_cols,
+                    int32 output_rows,
+                    int32 output_cols,
+                    int32 parallel_imgs,
+                    int32 offset_groups
+            ) {
+                auto num_kernels = input_channels * output_rows * output_cols * parallel_imgs;
+
+                for (auto k=0; k<num_kernels; k++) {
+                    const auto current_output_row = k % output_cols;
+                    const auto current_output_col = (k / output_cols) % output_rows;
+                    const auto current_batch = (k / (output_rows * output_cols)) % parallel_imgs;
+                    const auto current_input_channel = k / (output_rows * output_cols * parallel_imgs);
+                    const auto current_output_channel = current_input_channel * filter_rows * filter_cols;
+
+                    const auto group_index = current_input_channel / (input_channels / offset_groups);
+
+                    auto input_tensor_chipped = input_tensor.chip(current_batch, 0).chip(current_input_channel, 0);
+                    EigenConstTensor<Dtype, 5> offset_tensor_chipped = offset_tensor.chip(current_batch, 0).chip(group_index, 0);
+                    EigenConstTensor<Dtype, 4> mask_tensor_chipped = mask_tensor.chip(current_batch, 0).chip(group_index, 0);
+
+                    auto column_buffer_tensor_batch = current_batch;
+                    for (auto current_filter_row=0; current_filter_row < filter_rows; current_filter_row++) {
+                        for (auto current_filter_col=0; current_filter_col < filter_cols; current_filter_col++) {
+                            auto offset_h = offset_tensor_chipped(current_filter_row, current_filter_col, 0, current_output_row, current_output_col);
+                            auto offset_w = offset_tensor_chipped(current_filter_row, current_filter_col, 1, current_output_row, current_output_col);
+
+                            auto mask = mask_tensor_chipped(current_filter_row, current_filter_col, current_output_row, current_output_col);
+
+                            auto y = (current_output_row * stride_rows - padding_rows) + current_filter_row * dilation_rows + offset_h;
+                            auto x = (current_output_col * stride_cols - padding_cols) + current_filter_col * dilation_cols + offset_w;
+
+                            column_buffer_tensor(
+                                    current_output_channel,
+                                    column_buffer_tensor_batch,
+                                    current_output_row,
+                                    current_output_col
+                            ) = mask * bilinear_interpolate<Dtype>(input_tensor_chipped, y, x);
+                            column_buffer_tensor_batch++;
+                        }
+                    }
+                }
+
+            }
+
             template<typename Dtype>
             struct DeformableConv2DFunctor<CPUDevice, Dtype> {
                 Status operator()(OpKernelContext *context, typename TTypes<Dtype, 4>::ConstTensor input_tensor,
@@ -61,7 +170,7 @@ namespace tensorflow {
                                   typename TTypes<Dtype, 1>::ConstTensor bias_tensor,
                                   typename TTypes<Dtype, 4>::ConstTensor offset_tensor,
                                   typename TTypes<Dtype, 4>::ConstTensor mask_tensor,
-                                  typename TTypes<Dtype, 2>::Tensor column_buffer_tensor,
+                                  typename TTypes<Dtype, 4>::Tensor column_buffer_tensor,
                                   typename TTypes<Dtype, 4>::Tensor output_tensor,
                                   int32 input_batches,
                                   int32 input_channels,
@@ -79,60 +188,97 @@ namespace tensorflow {
                                   int32 output_channels,
                                   int32 output_rows,
                                   int32 output_cols,
-                                  int32 n_parallel_imgs,
+                                  int32 parallel_imgs,
                                   int32 weight_groups,
                                   int32 offset_groups
                 ) {
-                    auto batches = input_batches / n_parallel_imgs;
+                    auto batches = input_batches / parallel_imgs;
                     auto input_tensor_reshaped = input_tensor.reshape(
-                            Shape5D({batches, n_parallel_imgs, input_channels, input_rows, input_cols})
+                            Shape5D({batches, parallel_imgs, input_channels, input_rows, input_cols})
                     );
                     auto filter_tensor_reshaped = filter_tensor.reshape(
                             Shape5D({weight_groups, output_channels / weight_groups, filter_channels, filter_rows,
                                      filter_cols})
                     );
 
-                    auto filter_area = filter_rows * filter_cols * offset_groups;
                     auto offset_tensor_reshaped = offset_tensor.reshape(
-                            Shape5D({batches, n_parallel_imgs, 2 * filter_area, output_rows, output_cols})
+                            Shape8D({batches, parallel_imgs, offset_groups, filter_rows, filter_cols, 2, output_rows, output_cols})
                     );
                     auto mask_tensor_reshaped = mask_tensor.reshape(
-                            Shape5D({batches, n_parallel_imgs, filter_area, output_rows, output_cols})
+                            Shape7D({batches, parallel_imgs, offset_groups, filter_rows, filter_cols, output_rows, output_cols})
                     );
 
                     auto output_tensor_reshaped = output_tensor.reshape(
                             Shape5D({batches, weight_groups, output_channels / weight_groups,
-                                     n_parallel_imgs * output_rows, output_cols})
+                                     parallel_imgs * output_rows, output_cols})
                     );
 
                     // input_channels * filter_rows * filter_cols / weight_groups == filter_channels * filter_rows * filter_cols
-                    int32 elems = filter_channels * filter_rows * filter_cols;
-                    int32 cols = n_parallel_imgs * output_rows * output_cols;
+                    auto elems = filter_channels * filter_rows * filter_cols;
+                    auto cols = parallel_imgs * output_rows * output_cols;
 
                     auto column_buffer_tensor_reshaped = column_buffer_tensor.reshape(
                             Shape3D({weight_groups, elems, cols}));
 
-                    for (int32 b = 0; b < batches; b++) {
+                    for (auto b = 0; b < batches; b++) {
                         auto input_tensor_reshaped_batch = input_tensor_reshaped.chip(b, 0);
                         auto offset_tensor_reshaped_batch = offset_tensor_reshaped.chip(b, 0);
+                        auto mask_tensor_reshaped_batch = mask_tensor_reshaped.chip(b, 0);
                         auto output_tensor_reshaped_batch = output_tensor_reshaped.chip(b, 0);
 
-                        for (int g = 0; g < weight_groups; g++) {
+                        deformable_im2col<Dtype>(
+                                input_tensor_reshaped_batch,
+                                offset_tensor_reshaped_batch,
+                                mask_tensor_reshaped_batch,
+                                column_buffer_tensor,
+                                input_channels,
+                                filter_rows,
+                                filter_cols,
+                                padding_rows,
+                                padding_cols,
+                                stride_rows,
+                                stride_cols,
+                                dilation_rows,
+                                dilation_cols,
+                                output_rows,
+                                output_cols,
+                                parallel_imgs,
+                                offset_groups
+                        );
+
+                        for (auto g = 0; g < weight_groups; g++) {
                             int32 rows = output_channels / weight_groups;
 
-                            EigenTensor<Dtype, 2> filter_mtx = filter_tensor_reshaped.chip(g, 0).reshape(Shape2D({rows, elems}));
+                            EigenConstTensor<Dtype, 2> filter_mtx = filter_tensor_reshaped.chip(g, 0).reshape(
+                                    Shape2D({rows, elems}));
                             EigenTensor<Dtype, 2> column_buffer_mtx = column_buffer_tensor_reshaped.chip(g, 0);
 
                             auto mtx_shape = Shape2D({rows, cols});
-                            Eigen::array<Eigen::IndexPair<int>, 1> product_dims = { Eigen::IndexPair<int>(1, 0) };
+                            Eigen::array<Eigen::IndexPair<int>, 1> product_dims = {Eigen::IndexPair<int>(1, 0)};
 
                             EigenTensor<Dtype, 2> output_mtx = output_tensor_reshaped_batch.chip(g, 0).reshape(
                                     mtx_shape);
                             EigenTensor<Dtype, 2> mul = filter_mtx.contract(column_buffer_mtx, product_dims);
 
-                            output_mtx = output_mtx + mul;
+                            output_mtx += mul;
                         }
                     }
+
+                    auto output_tensor_transposed = output_tensor_reshaped.reshape(
+                            Shape5D({batches, output_channels, parallel_imgs, output_rows, output_cols})
+                    )
+                            .shuffle(Shape5D({0, 2, 1, 3, 4}))
+                            .reshape(Shape4D({input_batches, output_channels, output_rows, output_cols}));
+
+                    output_tensor = output_tensor_transposed.eval();
+
+                    auto bias_tensor_broadcasted = bias_tensor
+                            .reshape(Shape4D({1, output_channels, 1, 1}))
+                            .broadcast(Shape4D({input_batches, 1, output_rows, output_cols}));
+
+                    output_tensor += bias_tensor_broadcasted;
+
+                    return Status::OK();
                 }
             };
 
@@ -184,10 +330,7 @@ namespace tensorflow {
                 auto stride_rows = strides[0];
                 auto stride_cols = strides[1];
 
-                auto n_parallel_imgs = get_greatest_divisor_below_bound(input_batches);
-
-                auto filter_rows_eff = filter_rows + (filter_rows - 1) * (dilation_rows - 1);
-                auto filter_cols_eff = filter_cols + (filter_cols - 1) * (dilation_cols - 1);
+                auto parallel_imgs = get_greatest_divisor_below_bound(input_batches);
 
                 int64 output_rows, output_cols;
                 int64 padding_rows, padding_cols;
@@ -199,7 +342,7 @@ namespace tensorflow {
                                                        &output_cols, &padding_cols));
 
                 TensorShape column_buffer_shape(
-                        {input_channels * filter_rows * filter_cols, n_parallel_imgs * output_rows * output_cols});
+                        {input_channels * filter_rows * filter_cols, parallel_imgs, output_rows, output_cols});
                 Tensor column_buffer_tensor;
                 OP_REQUIRES_OK(context,
                                context->allocate_temp(DataTypeToEnum<T>::value,
@@ -217,7 +360,7 @@ namespace tensorflow {
                 Status s = deformableConv2DFunc(context, input_tensor.tensor<T, 4>(),
                                                 filter_tensor.tensor<T, 4>(), bias_tensor.tensor<T, 1>(),
                                                 offset_tensor.tensor<T, 4>(), mask_tensor.tensor<T, 4>(),
-                                                column_buffer_tensor.tensor<T, 2>(), output_tensor->tensor<T, 4>(),
+                                                column_buffer_tensor.tensor<T, 4>(), output_tensor->tensor<T, 4>(),
                                                 input_batches,
                                                 input_channels,
                                                 input_rows,
@@ -234,7 +377,7 @@ namespace tensorflow {
                                                 output_channels,
                                                 output_rows,
                                                 output_cols,
-                                                n_parallel_imgs,
+                                                parallel_imgs,
                                                 weight_groups,
                                                 offset_groups);
 
---
+++ tensorflow_addons/layers/deformable_conv2d.py
@@ -0,0 +1,204 @@
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+import typing
+
+import tensorflow as tf
+from typeguard import typechecked
+from tensorflow_addons.utils import types
+from tensorflow_addons.utils.resource_loader import LazySO
+from tensorflow.python.keras.utils import conv_utils
+
+_deformable_conv2d_ops_so = LazySO('custom_ops/layers/_deformable_conv2d_ops.so')
+
+
+@typechecked
+def _deformable_conv2d(
+        input: tf.Tensor,
+        filter: tf.Tensor,
+        bias: tf.Tensor,
+        offset: tf.Tensor,
+        mask: tf.Tensor,
+        strides: typing.Union[tuple, list],
+        dilations: typing.Union[tuple, list],
+        weight_groups: int,
+        offset_groups: int,
+        no_bias: bool,
+        padding: str,
+):
+    return _deformable_conv2d_ops_so.ops.addons_deformable_conv2d(
+        input=input,
+        filter=filter,
+        bias=bias,
+        offset=offset,
+        mask=mask,
+        strides=strides,
+        weight_groups=weight_groups,
+        offset_groups=offset_groups,
+        no_bias=no_bias,
+        padding=padding,
+        data_format='NCHW',
+        dilations=dilations,
+    )
+
+
+@tf.keras.utils.register_keras_serializable(package="Addons")
+class DeformableConv2D(tf.keras.layers.Layer):
+    @typechecked
+    def __init__(
+            self,
+            filters: int,
+            kernel_size: typing.Union[int, tuple, list] = (3, 3),
+            strides: typing.Union[int, tuple, list] = (1, 1),
+            padding: str = "valid",
+            data_format: str = "channels_first",
+            dilation_rate: typing.Union[int, tuple, list] = (1, 1),
+            weight_groups: int = 1,
+            offset_groups: int = 1,
+            use_deformable_conv_bias: bool = False,
+            use_filter_conv_bias: bool = False,
+            use_mask_conv_bias: bool = False,
+            kernel_initializer: types.Initializer = None,
+            bias_initializer: types.Initializer = None,
+            kernel_regularizer: types.Regularizer = None,
+            bias_regularizer: types.Regularizer = None,
+            kernel_constraint: types.Constraint = None,
+            bias_constraint: types.Constraint = None,
+            **kwargs
+    ):
+        super(DeformableConv2D, self).__init__(**kwargs)
+
+        self.filters = filters
+        self.kernel_size = conv_utils.normalize_tuple(kernel_size, 2, 'kernel_size')
+        self.strides = conv_utils.normalize_tuple(strides, 2, 'strides')
+        self.padding = conv_utils.normalize_padding(padding)
+        self.data_format = conv_utils.normalize_data_format(data_format)
+        self.dilation_rate = conv_utils.normalize_tuple(dilation_rate, 2, 'dilation_rate')
+        self.weight_groups = weight_groups
+        self.offset_groups = offset_groups
+        self.use_deformable_conv_bias = use_deformable_conv_bias
+        self.use_filter_conv_bias = use_filter_conv_bias
+        self.use_mask_conv_bias = use_mask_conv_bias
+        self.kernel_initializer = tf.keras.initializers.get(kernel_initializer)
+        self.bias_initializer = tf.keras.initializers.get(bias_initializer)
+        self.kernel_regularizer = tf.keras.regularizers.get(kernel_regularizer)
+        self.bias_regularizer = tf.keras.regularizers.get(bias_regularizer)
+        self.kernel_constraint = tf.keras.constraints.get(kernel_constraint)
+        self.bias_constraint = tf.keras.constraints.get(bias_constraint)
+
+        if self.padding == 'causal':
+            raise ValueError('Causal padding is not supported.')
+
+        if self.data_format != 'channels_first':
+            raise ValueError('`channels_last` data format is not supported.')
+
+        self.conv_offset = tf.keras.layers.Conv2D(
+            self.offset_groups * 2 * self.kernel_size[0] * self.kernel_size[1],
+            kernel_size=self.kernel_size,
+            strides=(1, 1),
+            padding=self.padding,
+            use_bias=self.use_filter_conv_bias,
+            data_format='channels_first',
+            kernel_initializer=self.kernel_initializer,
+            bias_initializer=self.bias_initializer,
+            kernel_regularizer=self.kernel_regularizer,
+            bias_regularizer=self.bias_regularizer,
+            kernel_constraint=self.kernel_constraint,
+            bias_constraint=self.bias_constraint,
+        )
+
+        self.conv_mask = tf.keras.layers.Conv2D(
+            self.offset_groups * self.kernel_size[0] * self.kernel_size[1],
+            kernel_size=self.kernel_size,
+            strides=(1, 1),
+            padding=self.padding,
+            use_bias=self.use_mask_conv_bias,
+            data_format='channels_first',
+            kernel_initializer=self.kernel_initializer,
+            bias_initializer=self.bias_initializer,
+            kernel_regularizer=self.kernel_regularizer,
+            bias_regularizer=self.bias_regularizer,
+            kernel_constraint=self.kernel_constraint,
+            bias_constraint=self.bias_constraint,
+        )
+
+        self.filter_weights = None
+        self.bias_weights = None
+
+    def build(self, input_shape):
+        kernel_shape = (self.filters, input_shape[1], self.kernel_size[0], self.kernel_size[1])
+
+        self.filter_weights = self.add_weight(
+            name='filter', shape=kernel_shape,
+            initializer=self.kernel_initializer, regularizer=self.kernel_regularizer,
+            constraint=self.kernel_constraint, trainable=True
+        )
+        self.bias_weights = self.add_weight(
+            name='bias', shape=(self.filters,),
+            initializer=self.bias_initializer, regularizer=self.bias_regularizer,
+            constraint=self.bias_constraint, trainable=True
+        )
+
+        self.built = True
+
+    def compute_output_shape(self, input_shape):
+        input_shape = tf.TensorShape(input_shape).as_list()
+
+        space = input_shape[2:]
+        new_space = []
+
+        for i, s in enumerate(space):
+            new_dim = conv_utils.conv_output_length(
+                s, self.kernel_size[i], padding=self.padding,
+                stride=self.strides[i], dilation=self.dilation_rate[i]
+            )
+            new_space.append(new_dim)
+
+        return tf.TensorShape([input_shape[0], self.filters] + new_space)
+
+    def call(self, inputs, **kwargs):
+        offset = self.conv_offset(inputs)
+        mask = tf.keras.activations.sigmoid(self.conv_mask(inputs))
+
+        return _deformable_conv2d(
+            input=inputs,
+            filter=self.filter_weights,
+            bias=self.bias_weights,
+            offset=offset,
+            mask=mask,
+            strides=self.strides,
+            weight_groups=self.weight_groups,
+            offset_groups=self.offset_groups,
+            no_bias=not self.use_deformable_conv_bias,
+            padding='SAME' if self.padding == 'same' else 'VALID',
+            dilations=self.dilation_rate,
+        )
+
+    def get_config(self):
+        config = {
+            "kernel_size": self.kernel_size,
+            "filters": self.filters,
+            "strides": self.strides,
+            "padding": self.padding,
+            "data_format": self.data_format,
+            "dilation_rate": self.dilation_rate,
+            "weight_groups": self.weight_groups,
+            "offset_groups": self.offset_groups,
+            "use_deformable_conv_bias": self.use_deformable_conv_bias,
+            "use_filter_conv_bias": self.use_filter_conv_bias,
+            "use_mask_conv_bias": self.use_mask_conv_bias,
+        }
+        base_config = super().get_config()
+        return {**base_config, **config}
