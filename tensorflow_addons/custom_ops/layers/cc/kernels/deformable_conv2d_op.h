c908073c8fb5ebcd14fffd80829f20c21e2ef45e
Licht Takeuchi <licht-t@outlook.jp>
2020-10-04 12:17:37 +0900

    foo

--- tensorflow_addons/custom_ops/layers/cc/kernels/deformable_conv2d_op.h
+++ tensorflow_addons/custom_ops/layers/cc/kernels/deformable_conv2d_op.h
@@ -1,20 +1,20 @@
-/* Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+// Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// =============================================================================
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-
-#ifndef TENSORFLOW_ADDONS_LAYERS_KERNELS_DEFORMABLE_CONV_OP_H_
-#define TENSORFLOW_ADDONS_LAYERS_KERNELS_DEFORMABLE_CONV_OP_H_
+#ifndef TENSORFLOW_ADDONS_LAYERS_KERNELS_DEFORMABLECONV2D_OP_H_
+#define TENSORFLOW_ADDONS_LAYERS_KERNELS_DEFORMABLECONV2D_OP_H_
 
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/util/tensor_format.h"
@@ -106,4 +106,4 @@ static inline int get_parallel_imgs(int n) {
 }  // namespace addons
 }  // namespace tensorflow
 
-#endif  // TENSORFLOW_ADDONS_LAYERS_KERNELS_DEFORMABLE_CONV_OP_H_
+#endif  // TENSORFLOW_ADDONS_LAYERS_KERNELS_DEFORMABLECONV2D_OP_H_
